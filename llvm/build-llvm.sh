2a8a94a2fef2af02e3d1fe846bcd3b5fe29245a9
Yizhuo <yzhai003@ucr.edu>
2020-06-24 16:32:35 -0700

    ingore the installation

--- llvm/build-llvm.sh
+++ llvm/build-llvm.sh
@@ -10,9 +10,9 @@ cd build
 cmake -G "Unix Makefiles" -DLLVM_ENABLE_RTTI=ON -DCMAKE_BUILD_TYPE=Debug ../llvm.src
 make
 
-if [ ! -d "$CUR_DIR/prefix" ]; then
-  mkdir $CUR_DIR/prefix
-fi
+#if [ ! -d "$CUR_DIR/prefix" ]; then
+#  mkdir $CUR_DIR/prefix
+#fi
 
-cmake -DCMAKE_INSTALL_PREFIX=$CUR_DIR/prefix -P cmake_install.cmake
+#cmake -DCMAKE_INSTALL_PREFIX=$CUR_DIR/prefix -P cmake_install.cmake
 
